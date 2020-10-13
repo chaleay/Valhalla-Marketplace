395eb6ecc9905dcf5840fc75f369c970d3dc74fb
chaleay <elijahgaytan@utexas.edu>
2020-10-12 19:23:35 -0500

    finished frontend basics: product page, main page

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Rating = ({ value, text }) => {
+const Rating = ({ value, text, color }) => {
   return (
     <div className="rating">
       <span>
         <i
+          style={{ color: color }}
           className={
             value >= 1
               ? 'fas fa-star'
@@ -16,6 +18,7 @@ const Rating = ({ value, text }) => {
       </span>
       <span>
         <i
+          style={{ color: color }}
           className={
             value >= 2
               ? 'fas fa-star'
@@ -27,17 +30,19 @@ const Rating = ({ value, text }) => {
       </span>
       <span>
         <i
+          style={{ color: color }}
           className={
             value >= 3
               ? 'fas fa-star'
               : value >= 2.5
-              ? 'fas fa-star-half alt'
+              ? 'fas fa-star-half-alt'
               : 'far fa-star'
           }
         ></i>
       </span>
       <span>
         <i
+          style={{ color: color }}
           className={
             value >= 4
               ? 'fas fa-star'
@@ -49,6 +54,7 @@ const Rating = ({ value, text }) => {
       </span>
       <span>
         <i
+          style={{ color: color }}
           className={
             value >= 5
               ? 'fas fa-star'
@@ -58,8 +64,20 @@ const Rating = ({ value, text }) => {
           }
         ></i>
       </span>
+      <span className="pl-2">{text && text}</span>
     </div>
   );
+};
+
+Rating.defaultProps = {
+  color: '#f8e825',
+};
+
+//establish propTypes for passing in props
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
 };
 
 export default Rating;
